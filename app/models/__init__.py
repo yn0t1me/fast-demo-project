7091b36f3a07b4fb045375a0d18df22a4ff05147
yn0t1me <yn0t1me@outlook.com>
2025-08-22 19:31:50 +0800

    fix: 注册hero模型类

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,7 +7,8 @@ from .user import User
 # 如果未来有其他模型，比如 Post, Item，也在这里导入
 # from .post import Post
 # from .item import Item
+from .heroes import Hero
 
 # 可选：使用 __all__ 来明确声明这个包对外暴露的接口
 # 这是一种良好的编程习惯
-__all__ = ["Base", "User"]
+__all__ = ["Base", "User", "Hero"]
